53c90db6caa8f38538fd52d566dc3bb6de3e55ec
drakontia <mhden@drakontia.com>
2017-06-10 19:48:10 +0900

    Initial commit

---
+++ app/src/main/kotlin/com/drakontia/qiitaclient/model/User.kt
@@ -0,0 +1,9 @@
+package com.drakontia.qiitaclient.model
+
+/**
+ * Created by drakontia on 2017/06/10.
+ */
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String
+)
